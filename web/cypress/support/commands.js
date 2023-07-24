368d20e57b128e445651cbc5fde8c0540392d5ea
Rudá <ruda_soares123@hotmail.com>
2023-07-24 00:12:50 -0300

    meu primeiro commit

---
+++ web/cypress/support/commands.js
@@ -0,0 +1,56 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+// Cypress.Commands.add('doLogin', (user) => {
+//     cy.visit('http://localhost:3000')
+
+//     if (user.email) {
+//         cy.get('input[name=email]').type(user.email)
+//     }
+//     if (user.password) {
+//         cy.get('input[name=password]').type(user.password)
+//     }
+
+//     cy.contains('button', 'Entrar').click()
+// })
+
+// Cypress.Commands.add('popUpHave', (text) => {
+//     cy.get('#swal2-content')
+//         .should('be.visible')
+//         .should('have.text', text)
+// })
+import users from '../fixtures/users.json'
+
+import loginPage from './pages/LoginPage'
+import studentPage from './pages/StudentPage'
+
+Cypress.Commands.add('adminLogin', ()=>{
+    const user = users.admin
+
+    loginPage.doLogin(user)
+    studentPage.navbar.userLoggedIn(user.name)
+   
+})
